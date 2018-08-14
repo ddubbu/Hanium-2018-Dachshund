1d9cbc5e36af78274376788f930b929fefd4943e
DaJungYu <tender5688@gmail.com>
2018-08-14 15:28:37 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Root from './pages/Root';
+import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
